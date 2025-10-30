5b803dbc9c03406e07f87f74349d03a68535825c
SamaD-01 <abdessamad.ajja00@gmail.com>
2025-10-30 11:03:16 +0100

    Testing Grande Suite Case

--- index.js
+++ index.js
@@ -13,12 +13,30 @@ const lancersYams = (...params) => {
 const calculatePoints = (diceRole) => {
     if (diceRole.length != 5 )  return null;
     let points = 0;
+    const GRANDE_SUITE_CASE_1 = [1, 2, 3, 4, 5];
+    const GRANDE_SUITE_CASE_2 = [2, 3, 4, 5, 6];
+
     // Yams Case
     if (diceRole[0] === diceRole[1] && diceRole[1] === diceRole[2] && diceRole[2] === diceRole[3] && diceRole[3] === diceRole[4]) {
-        points = 50;
+        points += 50;
+    }
+    // Grande Suite Case
+    const isGrandeSuitePattern = (array, pattern) => {
+        for (let i = 0; i < pattern.length; i++) {
+            if (!array.includes(pattern[i])) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    if (isGrandeSuitePattern(diceRole, GRANDE_SUITE_CASE_1) || isGrandeSuitePattern(diceRole, GRANDE_SUITE_CASE_2)) {
+        points += 25;
     }
 
     return points;
 }
 
 export { lancersYams };
+
+
--- index.test.js
+++ index.test.js
@@ -4,8 +4,11 @@ import { lancersYams } from ".";
 describe("Yams game", () => {
     it.each([
         [null, [[1, 1, 1, 1, 1, 1]]],
+        [null, [[7, 1, 1, 1, 1]]],
         [50, [[1, 1, 1, 1, 1]]],
         [100, [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]],
+        [75, [[1, 1, 1, 1, 1], [2, 3, 4, 5, 6]]],
+        [50, [[1, 2, 3, 4, 5], [2, 3, 4, 5, 6]]],
     ])("Should return %s points, for %s dice roles", (expectedPoints, diceRoles) => {
         expect(lancersYams(diceRoles)).toBe(expectedPoints);
     })
