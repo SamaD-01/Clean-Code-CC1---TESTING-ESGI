51cb43960de1b55de1b164c228a98cd55f78a704
SamaD-01 <abdessamad.ajja00@gmail.com>
2025-10-30 10:47:06 +0100

    Testing Yams case

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 
 const lancersYams = (...params) => {
     let diceRoles = params[0];
+    let totalPoints = 0;
     for (let diceRole of diceRoles) {
-        if (diceRole.length != 5 ) {
-            return null;
-        }
+        let points = calculatePoints(diceRole);
+        if (points === null) return null;
+        totalPoints += points;
     }
+    return totalPoints;
 }
+
+const calculatePoints = (diceRole) => {
+    if (diceRole.length != 5 )  return null;
+    let points = 0;
+    // Yams Case
+    if (diceRole[0] === diceRole[1] && diceRole[1] === diceRole[2] && diceRole[2] === diceRole[3] && diceRole[3] === diceRole[4]) {
+        points = 50;
+    }
+
+    return points;
+}
+
 export { lancersYams };
--- index.test.js
+++ index.test.js
@@ -5,6 +5,7 @@ describe("Yams game", () => {
     it.each([
         [null, [[1, 1, 1, 1, 1, 1]]],
         [50, [[1, 1, 1, 1, 1]]],
+        [100, [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]],
     ])("Should return %s points, for %s dice roles", (expectedPoints, diceRoles) => {
         expect(lancersYams(diceRoles)).toBe(expectedPoints);
     })
